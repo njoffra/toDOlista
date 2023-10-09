d88d74cccceaa391e1cc473152c52c8cd21557bd
njoffra <markic.franjo.21@size.ba>
2023-10-10 00:42:58 +0200

    little fix

--- toDOlist/toDOlist.py
+++ toDOlist/toDOlist.py
@@ -4,14 +4,14 @@ from tkinter import messagebox
 import sqlite3 as sql
 
 def add_task():
-    task_string = task_text.get("1.0", "end-1c") 
+    task_string = task_text.get("1.0", "end-1c")
     if len(task_string) == 0:
         messagebox.showinfo('Error', 'Field is Empty.')
     else:
         tasks.append(task_string)
         the_cursor.execute('insert into tasks values (?)', (task_string,))
         list_update()
-        task_text.delete("1.0", "end")  
+        task_text.delete("1.0", "end")
 
 def list_update():
     clear_list()
@@ -51,20 +51,20 @@ def retrieve_database():
 
 if __name__ == "__main__":
     guiWindow = tk.Tk()
-    guiWindow.title("top toDO lista nadrealista")
-    guiWindow.geometry("700x450+750+250")  
+    guiWindow.title("toDO top lista nadreaLISTA")
+    guiWindow.geometry("700x450+750+250")
     guiWindow.resizable(0, 0)
-    guiWindow.configure(bg="#3498db")  
+    guiWindow.configure(bg="#3498db")
 
     the_connection = sql.connect('listOfTasks.db')
     the_cursor = the_connection.cursor()
-    the_cursor.execute('create table (title text)')
+    the_cursor.execute('create table if not exists tasks (title text)')
 
     tasks = []
 
-    header_frame = tk.Frame(guiWindow, bg="#3498db")  
-    functions_frame = tk.Frame(guiWindow, bg="#3498db")  
-    listbox_frame = tk.Frame(guiWindow, bg="#3498db")  
+    header_frame = tk.Frame(guiWindow, bg="#3498db")
+    functions_frame = tk.Frame(guiWindow, bg="#3498db")
+    listbox_frame = tk.Frame(guiWindow, bg="#3498db")
 
     header_frame.pack(fill="both")
     functions_frame.pack(side="left", expand=True, fill="both")
@@ -72,10 +72,10 @@ if __name__ == "__main__":
 
     header_label = ttk.Label(
         header_frame,
-        text="toDO lista",
+        text="toDolista",
         font=("Consolas", "30"),
-        background="#3498db",  
-        foreground="#ffffff"  
+        background="#3498db",
+        foreground="#ffffff"
     )
 
     header_label.pack(padx=20, pady=20)
@@ -84,66 +84,66 @@ if __name__ == "__main__":
         functions_frame,
         text="Enter the Task:",
         font=("Consolas", "11", "bold"),
-        background="#3498db",  
-        foreground="#ffffff"  
+        background="#3498db",
+        foreground="#ffffff"
     )
 
     task_label.place(x=30, y=40)
 
     task_text = tk.Text(
         functions_frame,
-        font=("Consolas", "10"),  
-        width=40,  
-        height=5,  
-        background="#ffffff",  
-        foreground="#000000"  
+        font=("Consolas", "10"),
+        width=40,
+        height=5,
+        background="#ffffff",
+        foreground="#000000"
     )
 
-    task_text.place(x=30, y=80)  
+    task_text.place(x=30, y=80)
 
     add_button = ttk.Button(
         functions_frame,
         text="Add Task",
-        width=30,  
+        width=30,
         command=add_task,
-        style="TButton"  
+        style="TButton"
     )
     del_button = ttk.Button(
         functions_frame,
         text="Delete Task",
-        width=30,  
+        width=30,
         command=delete_task,
-        style="TButton"  
+        style="TButton"
     )
     del_all_button = ttk.Button(
         functions_frame,
         text="Delete All Tasks",
-        width=30,  
+        width=30,
         command=delete_all_tasks,
-        style="TButton"  
+        style="TButton"
     )
     exit_button = ttk.Button(
         functions_frame,
         text="Exit",
-        width=30,  
+        width=30,
         command=close,
-        style="TButton"  
+        style="TButton"
     )
 
-    add_button.place(x=30, y=210)  
+    add_button.place(x=30, y=210)
     del_button.place(x=30, y=250)
     del_all_button.place(x=30, y=290)
-    exit_button.place(x=30, y=330)  
+    exit_button.place(x=30, y=330)
 
     task_listbox = tk.Listbox(
         listbox_frame,
-        width=40,  
+        width=40,
         height=13,
         selectmode='SINGLE',
-        background="#ffffff",  
-        foreground="#000000",  
-        selectbackground="#3498db",  
-        selectforeground="#ffffff"  
+        background="#ffffff",
+        foreground="#000000",
+        selectbackground="#3498db",
+        selectforeground="#ffffff"
     )
 
     task_listbox.place(x=10, y=20)
